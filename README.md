14d800f1ae6a0b9e8d816069b108c204d0412a69
Emily Curry <emily.curry@illumisoft.com>
2018-12-14 14:03:12 -0600

    v0.45.6

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # metro-minify-uglify
-This package modifies the metro minifier, disabling name-mangling. Why would you want that? How did you even get here? All good questions. 
 
-For use with the version of metro (0.30.2) that the latest version of expo (30.0.0) depends on. 
+This package modifies the metro minifier, disabling name-mangling. Why would you want that? How did you even get here? All good questions.
+
+For use with the version of metro (0.45.6) that the latest version of expo (31.0.0) depends on.
--- src/minifier.js
+++ src/minifier.js
@@ -1,55 +1,55 @@
 /**
- * Copyright (c) 2016-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * 
+ *  strict-local
  * @format
  */
 
-'use strict';
+"use strict";
 
-const uglify = require('uglify-es');
-
-
-
-
+const uglify = require("uglify-es");
 
 function noSourceMap(code) {
-  return minify(code).code;
+  let options =
+    arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  return minify(code, undefined, options).code;
 }
 
-function withSourceMap(
-code,
-sourceMap,
-filename)
-{
-  const result = minify(code, sourceMap);
-
+function withSourceMap(code, sourceMap, filename) {
+  let options =
+    arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
+  const result = minify(code, sourceMap, options);
   const map = JSON.parse(result.map);
+
   map.sources = [filename];
+
   return { code: result.code, map };
 }
 
-function minify(inputCode, inputMap) {
+function minify(inputCode, inputMap, options) {
   const result = uglify.minify(inputCode, {
     mangle: false,
+
     output: {
       ascii_only: true,
       quote_style: 3,
-      wrap_iife: true },
+      wrap_iife: true
+    },
 
     sourceMap: {
       content: inputMap,
-      includeSources: false },
+      includeSources: false
+    },
 
     toplevel: false,
     compress: {
       // reduce_funcs inlines single-use function, which cause perf regressions.
-      reduce_funcs: false } });
-
-
+      reduce_funcs: false
+    }
+  });
 
   if (result.error) {
     throw result.error;
@@ -57,13 +57,13 @@ function minify(inputCode, inputMap) {
 
   return {
     code: result.code,
-    map: result.map };
-
+    map: result.map
+  };
 }
 
 const metroMinifier = {
   noSourceMap,
-  withSourceMap };
-
+  withSourceMap
+};
 
 module.exports = metroMinifier;
